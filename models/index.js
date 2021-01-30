7b62ccf5696ffeb03424f9193359fa21e20c384d
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2021-01-30 07:17:29 -0500

    finished category routes

--- models/index.js
+++ models/index.js
@@ -4,17 +4,19 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category.... so each product is going to have a category_id, we state that the foreignKey on products will be the category_id
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
-// Categories have many Products
+// Categories have many Products... and the link between them is still going to be that the products will have a category_id, that foreign key reference will link them 
 
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
+
+// THROUGH TABLES... both product and tag share ownership of a product_tag, we created a ProductTag table to connect these by their primary keys.
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through: 'product_tag',
